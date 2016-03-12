8470c31bf5932af487e9d73d48c04484022db833
Florian Schulz <hanbei@hanbei.de>
2016-03-12 16:38:02 +0100

    Build with theme

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
-
-git config --global user.email "cwjoneill@gmail.com"
-git config --global user.name "Conor O'Neill"
-
 # Build the project.
-hugo # if using a theme, replace by `hugo -t <yourtheme>
+hugo -t casper # if using a theme, replace by `hugo -t <yourtheme>
