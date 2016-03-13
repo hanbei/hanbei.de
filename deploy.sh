b8f8ceddeb9ff6d4d0b5d01d0058ba03c24e6954
Florian Schulz <hanbei@hanbei.de>
2016-03-13 19:45:29 +0100

    Remove credentials

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-rm -rf public 
+rm -rf public
 hugo -t casper # if using a theme, replace by `hugo -t <yourtheme>
 cd public
-echo $FTP_USER
-echo $FTP_PASS
 find . -type f -exec curl --ftp-create-dirs -T {} -u $FTP_USER:$FTP_PASS ftp://ftp.hanbei.de/staging/{} \;
