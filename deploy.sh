33bd0af2fb7cafd87816c676ecc70c00339df692
Florian Schulz <schulz@re-media.eu>
2016-04-07 16:53:56 +0200

    Pull request is true if master

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ rm -rf public
 hugo -t casper # if using a theme, replace by `hugo -t <yourtheme>
 cd public
 echo ${TRAVIS_PULL_REQUEST}
-if [ "${TRAVIS_PULL_REQUEST}" = "true" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     find . -type f -exec curl --ftp-create-dirs -T {} -u $FTP_USER:$FTP_PASS ftp://ftp.hanbei.de/staging/{} \;
 else
-    echo "Working on master branch"    
+    echo "Working on master branch"
 fi
