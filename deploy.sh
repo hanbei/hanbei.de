c3b536a0661da06246922a6ab25a9a45859d8645
Florian Schulz <schulz@re-media.eu>
2016-04-07 16:46:56 +0200

    Deploy feature branches on staging

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,9 @@
 rm -rf public
 hugo -t casper # if using a theme, replace by `hugo -t <yourtheme>
 cd public
-find . -type f -exec curl --ftp-create-dirs -T {} -u $FTP_USER:$FTP_PASS ftp://ftp.hanbei.de/staging/{} \;
+echo ${TRAVIS_PULL_REQUEST}
+if [ "${TRAVIS_PULL_REQUEST}" = "true" ]; then
+    find . -type f -exec curl --ftp-create-dirs -T {} -u $FTP_USER:$FTP_PASS ftp://ftp.hanbei.de/staging/{} \;
+else
+    echo "Working on master branch"    
+fi
