0fb9e1fe48118a1a111eee0c429950970793ac88
Florian Schulz <schulz@re-media.eu>
2016-04-08 11:57:19 +0200

    Check branch name for continous deployment

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,9 @@ hugo -t casper # if using a theme, replace by `hugo -t <yourtheme>
 cd public
 echo ${TRAVIS_PULL_REQUEST}
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    find . -type f -exec curl --ftp-create-dirs -T {} -u $FTP_USER:$FTP_PASS ftp://ftp.hanbei.de/staging/{} \;
+    if [ "${TRAVIS_BRANCH}" != "master" ]; then
+        find . -type f -exec curl --ftp-create-dirs -T {} -u $FTP_USER:$FTP_PASS ftp://ftp.hanbei.de/staging/{} \;
+    fi
 else
     echo "Working on master branch"
 fi
